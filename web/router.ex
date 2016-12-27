a30333ac472bb13b14652b34ab662e2706a85a6e
Dan <dan@heroboyfriend.com>
2016-12-27 20:52:18 +0000

    use Ecto for db persistence

--- web/router.ex
+++ web/router.ex
@@ -17,8 +17,7 @@ defmodule Rumbl.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/users", 	UserController, :index
-    get "/users/:id",	UserController, :show
-    get "/", 		PageController, :index
+		resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
